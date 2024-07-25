e9c9b08f980e8d08c155913dd6c6d36701f37da6
Guestsss <akshaya151204@gmail.com>
2024-07-25 12:38:37 +0530

    Sum1

---
+++ src/ControlFlow/Sum.java
@@ -0,0 +1,15 @@
+package ControlFlow;
+
+public class Sum {
+    public static void main(String[] args) {
+        int sum = 0;
+        int number = 25;
+        int i;
+        for (i = 1; i < number; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        System.out.println("print Sum of even numbers" + sum);
+    }
+}
