f614f6ca16832510cb8f3751726c53bdd654cee1
Guestsss <akshaya151204@gmail.com>
2024-07-26 10:31:00 +0530

    Grade1

---
+++ src/ControlFlow/Grade1.java
@@ -0,0 +1,16 @@
+package ControlFlow;
+
+public class Grade1 {
+    public static void main(String[] args) {
+        int mark = 489;
+        if (mark <= 500 && mark > 400) {
+            System.out.println("Grade A");
+        } else if (mark <= 400 && mark > 300) {
+            System.out.println("Grade B");
+        } else if (mark >= 300 && mark < 200) {
+            System.out.println("Grade C");
+        } else {
+            System.out.println("FAIL");
+        }
+    }
+}
