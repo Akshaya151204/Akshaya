079f79394e2586cda85c6e9ec4928053bf99f8fb
Akshaya151204 <akshaya151204@gmail.com>
2024-07-30 15:01:23 +0530

    Update Largest.java

--- src/ControlFlow/Largest.java
+++ src/ControlFlow/Largest.java
@@ -2,18 +2,22 @@ package ControlFlow;
 
 public class Largest {
     public static void main(String[] args) {
-        int [] a={42,445547,78487,145142,78448};
+        int[] a = {42,56, 445547, 78487, 145142, 78448};
         int i;
         int j;
         int t;
-        for(i=0;i<=4;i++){
-            for(j=0;j<4;j++){
-                t=a[i];
-                a[i]=a[j];
-                a[j]=t;
+        for (i = 0; i < a.length; i++) {
+            for (j = 0; j < a.length; j++) {
+                if (a[i] < a[j]) {
+                    t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
             }
         }
-        System.out.println("Largest:"+a[4]);
-        System.out.println("Smallest:"+a[0]);
+        System.out.println("Largest:" + a[a.length-1]);
+
+        System.out.println("Smallest:" + a[0]);
     }
 }
+
