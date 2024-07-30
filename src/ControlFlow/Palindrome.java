711e237dfdddb573f06703cdd07f8421145b628c
Akshaya151204 <akshaya151204@gmail.com>
2024-07-30 14:23:22 +0530

    Update Palindrome.java

--- src/ControlFlow/Palindrome.java
+++ src/ControlFlow/Palindrome.java
@@ -4,17 +4,15 @@ public class Palindrome {
     public static void main(String[] args) {
         String[] a = {"racecar", "madam", "man", "question"};
         String b;
-        int i;
-        int j;
         char ch;
-        for (i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++) {
             b = "";
-            for (j = 0; j < a[i].length(); j++) {
+            for (int j = 0; j < a[i].length(); j++) {
                 ch = a[i].charAt(j);
                 b = ch + b;
             }
-            if (a[i].equals(a[i])) {
-                System.out.println("Palindrome" + a[i]);
+            if (a[i].equals(b)) {
+                System.out.println("Palindrome:" + a[i]);
             }
         }
     }
