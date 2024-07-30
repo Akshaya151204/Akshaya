370a1eca84b75242e60942dd8aa6cb07cad1e294
Guestsss <akshaya151204@gmail.com>
2024-07-30 15:17:40 +0530

    Array

--- NewTask1/Array.java
+++ NewTask1/Array.java
@@ -6,16 +6,16 @@ public class Array {
         int [] array2={850,750,450,250};
         int [] array3={450,250,350,471};
         int i,j,k;
-        int count=0;
         for(i=0;i<array1.length;i++){
             for(j=0;j<array2.length;j++){
                 for(k=0;k<array3.length;k++){
                     if(array1[i] == array2[j] && array2[j] == array3[k] && array3[k] == array1[i]){
-                        count++;
+                        System.out.println("Repeated numbers:"+array1[i]);
+
                     }
                 }
             }
         }
-        System.out.println("Repeated numbers:"+count);
+
     }
 }
