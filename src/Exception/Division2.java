6d99d22560ce23df9083d1815cc171fa351f0cb5
Guestsss <akshaya151204@gmail.com>
2024-08-09 16:55:55 +0530

    Exception Handling

---
+++ src/Exception/Division2.java
@@ -0,0 +1,21 @@
+package Exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Division2 {
+    public static void main(String[] args) {
+        Scanner divisor = new Scanner(System.in);
+        System.out.println("Enter A & B");
+
+        try {
+            int a = divisor.nextInt();
+            int b = divisor.nextInt();
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero" + e);
+        } catch (InputMismatchException ee) {
+            System.out.println("Invalid input");
+        }
+    }
+}
