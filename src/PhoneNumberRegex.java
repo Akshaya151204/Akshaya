5a5c93d67f76e2f1a4e0f9844c4cd35fa1ecf710
Guestsss <akshaya151204@gmail.com>
2024-09-17 17:32:40 +0530

    Regex

--- src/PhoneNumberRegex.java
+++ src/PhoneNumberRegex.java
@@ -1,31 +1,35 @@
 package src;
-import java.io.FilterOutputStream;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.lang.String;
 
 public class PhoneNumberRegex {
 
-    public class PhoneNumber {
-        public static String extractPhoneNumbers(String text) {
-            String phoneRegex = "\\(\\+\\d{2}\\)\\s\\d{5}-\\d{5} | \\d{3}-\\d{3}-\\d{4}";
-            Pattern pattern = Pattern.compile(phoneRegex);
-            Matcher matcher = pattern.matcher(text);
-            String result = "";
-            while (matcher.find()) {
-                result = result + matcher.group() + " or ";
+    public String extractPhoneNumbers(String text) {
+        String phoneRegex = "\\(\\+\\d{2}\\)\\s\\d{5}-\\d{5} | \\d{3}-\\d{3}-\\d{4}";
+        Pattern pattern = Pattern.compile(phoneRegex);
+        Matcher matcher = pattern.matcher(text);
+        StringBuilder result = new StringBuilder();
+        while (matcher.find()) {
+            if (!result.isEmpty()) {
+                result.append(" or ");
             }
-            if (result != "")
-                return result;
-            else
-                return "null";
+            result.append(matcher.group());
         }
+        if (!result.isEmpty()) {
+            return result.toString();
+        } else
+            return "null";
+    }
+
 
         public static void main(String[] args) {
+            PhoneNumberRegex phoneNumberRegex=new PhoneNumberRegex();
 
-            System.out.println(extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
-            System.out.println(extractPhoneNumbers("No phone number here!"));
-        }
+            System.out.println(phoneNumberRegex.extractPhoneNumbers("Call me at (+91) 23456-72890 or at 987-424-4124"));
+//            System.out.println(phoneNumberRegex.extractPhoneNumbers("No phone number here!"));
+     }
     }
-}
+
 
