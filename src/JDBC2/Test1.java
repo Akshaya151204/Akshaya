a4d02f1507e7cc92cc5b8544a60e48254c4497ba
Guestsss <akshaya151204@gmail.com>
2024-10-09 16:21:38 +0530

    New Address and Employee

--- src/JDBC2/Test1.java
+++ src/JDBC2/Test1.java
@@ -3,13 +3,15 @@ package src.JDBC2;
 public class Test1 {
     String firstName;
     String lastName;
+    String phoneNumber;
 
-    public Test1(String firstName, String lastName) {
+    public Test1(String firstName, String lastName,String phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.phoneNumber=phoneNumber;
     }
     public static void main(String[] args) {
-        Test1 test = new Test1("Akshaya", "Amuthan");
+        Test1 test = new Test1("Akshaya", "Amuthan","6728392839");
         System.out.println(test);
     }
 }
