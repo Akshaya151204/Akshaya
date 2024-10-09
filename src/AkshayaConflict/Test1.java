c1e3d6944742de17a1558995bb387b031983db01
Guestsss <akshaya151204@gmail.com>
2024-10-09 11:15:52 +0530

    New Address and Employee

---
+++ src/AkshayaConflict/Test1.java
@@ -0,0 +1,16 @@
+package src.AkshayaConflict;
+
+public class Test1 {
+    String firstName;
+    String lastName;
+
+    public Test1(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public static void main(String[] args) {
+        Test1 test = new Test1("Akshaya", "Amuthan");
+        System.out.println(test);
+    }
+}
