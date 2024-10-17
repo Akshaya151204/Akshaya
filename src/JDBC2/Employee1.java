50d37b8fac02da0f0af22e4ccfe51b7797d18968
Akshaya151204 <akshaya151204@gmail.com>
2024-10-17 10:42:28 +0530

    Update Employee1.java

--- src/JDBC2/Employee1.java
+++ src/JDBC2/Employee1.java
@@ -1,69 +1,98 @@
 package src.JDBC2;
-public class Employee1 {
+
+public class Employee1  {
+    String employeeId;
+    String firstName;
+    String lastName;
+    String middleName;
+    String dob;
+    String gender;
+    String emailAddress;
+    String phoneNumber;
+    public Address address;
     String jobTitle;
     String department;
-    String managerID;
+    String managerId;
+    String dateOfJoining;
     String employmentStatus;
-    String salary;
+    double salary;
     String workLocation;
     private String panNumber;
     private String aadhaarNumber;
     private String bankAccountDetails;
     String employmentEligibilityVerification;
-    String backgroundStatus;
+    String backgroundCheckStatus;
     String emergencyContact;
     String skills;
     String education;
     String certifications;
+    String performance;
+    Employee1(String employeeId, String firstName, String lastName
+            , String middleName, String dob, String gender, String emailAddress, String phoneNumber
+            , Address address,
+              String jobTitle, String department, String managerId, String dateOfJoining, String employmentStatus,
+              double salary, String workLocation, String employmentEligibilityVerification, String backgroundCheckStatus, String emergencyContact,
+              String skills, String education, String certifications, String performance) {
 
-    public void setPanNumber(String panNumber) {
-        this.panNumber = panNumber;
+        this.employeeId=employeeId;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.middleName=middleName;
+        this.dob=dob;
+        this.gender=gender;
+        this.emailAddress=emailAddress;
+        this.phoneNumber=phoneNumber;
+        this.address=address;
+        this.jobTitle=jobTitle;
+        this.department=department;
+        this.managerId=managerId;
+        this.dateOfJoining=dateOfJoining;
+        this.employmentStatus=employmentStatus;
+        this.salary=salary;
+        this.workLocation=workLocation;
+        this.employmentEligibilityVerification=employmentEligibilityVerification;
+        this.backgroundCheckStatus=backgroundCheckStatus;
+        this.emergencyContact=emergencyContact;
+        this.skills=skills;
+        this.education=education;
+        this.certifications=certifications;
+        this.performance=performance;
     }
-
-    public void setAadhaarNumber(String aadhaarNumber) {
-        this.aadhaarNumber = aadhaarNumber;
-    }
-
-    public void setBankAccountDetails(String bankAccountDetails) {
-        this.bankAccountDetails = bankAccountDetails;
-    }
-
     public String getPanNumber() {
         return panNumber;
     }
-
+    public void setPanNumber(String panNumber) {
+        this.panNumber = panNumber;
+    }
     public String getAadhaarNumber() {
         return aadhaarNumber;
     }
-
+    public void setAadhaarNumber(String aadhaarNumber) {
+        this.aadhaarNumber = aadhaarNumber;
+    }
     public String getBankAccountDetails() {
         return bankAccountDetails;
     }
-    public Employee1(String jobTitle, String department, String managerID, String employmentStatus, String salary, String workLocation, String employmentEligibilityVerification, String backgroundStatus, String emergencyContact, String skills, String education, String certifications, String performanceReviews) {
-        this.jobTitle = jobTitle;
-        this.department = department;
-        this.managerID = managerID;
-        this.employmentStatus = employmentStatus;
-        this.salary = salary;
-        this.workLocation = workLocation;
-        this.employmentEligibilityVerification = employmentEligibilityVerification;
-        this.backgroundStatus = backgroundStatus;
-        this.emergencyContact = emergencyContact;
-        this.skills = skills;
-        this.education = education;
-        this.certifications = certifications;
-        this.performanceReviews = performanceReviews;
+    public void setBankAccountDetails(String bankAccountDetails) {
+        this.bankAccountDetails = bankAccountDetails;
     }
-
-    String performanceReviews;
-
+    @Override
+    public String toString(){
+        return employeeId+"\n"+firstName+"\n"+lastName+"\n"+middleName+"\n"+dob+"\n"+gender+"\n"+emailAddress+"\n"+phoneNumber+"\n"+address+
+                jobTitle+"\n"+department+"\n"+managerId+"\n"+dateOfJoining+"\n"+employmentStatus+"\n"+
+                salary+"\n"+workLocation+"\n"+employmentEligibilityVerification+"\n"+backgroundCheckStatus+"\n"+emergencyContact+"\n"+
+                skills+"\n"+education+"\n"+certifications+"\n"+performance+"\n"+aadhaarNumber+"\n"+panNumber+"\n"+bankAccountDetails;
+    }
     public static void main(String[] args) {
-        Employee1 employee1=new Employee1("Employee","Application","CI3005","FullTime","50000",
-                "Medavakkam","Verification Done","Verification done","9382827282","Painting",
-                "BCA","NPTEL","Excellent");
-        System.out.println(employee1);
-        employee1.setPanNumber("FW456789");
-        employee1.setAadhaarNumber("ASDF5667");
-        employee1.setBankAccountDetails("Indian Bank");
+        Address address=new Address("Plot no 7","annamalai nagar","Kanchipura,","TamilNadu","631502");
+        Employee1 employee=new Employee1("cs1211","A","Amuthan","Akshaya","15/12/2004",
+                "Female","akshu19721@gmail.com","6383909012",address,"Senior UI Developer","Application","1441","10/7/2013",
+                "Full Time",70000,"Medavakkam","Verification Done","Verification Done"
+                ,"789090123","Frontend Development",
+                "BCA","Microsoft Certification","Excellent");
+        employee.setBankAccountDetails("Indian Bank");
+        employee.setAadhaarNumber("7872 297 2892");
+        employee.setPanNumber("120AA12903709");
+        System.out.println(employee);
     }
 }
